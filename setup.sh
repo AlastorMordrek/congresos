8b80c0d0789bab7f06f99a39bd7da6fd6f8d97e9
Silas <el.silas@pm.me>
2025-09-19 21:45:02 -0700

    Se agrego archivo de ejemplo para override de docker para ciclo de desarrollo y ergonomia de los desarrolladores.

--- setup.sh
+++ setup.sh
@@ -6,6 +6,16 @@ fi
 
 # Comprobar si debemos correr en modo desarrollo o produccion.
 if [ "$1" = "--dev" ]; then
+    if [ ! -f "docker-compose.override.yml" ]; then
+        if [ -f "docker-compose.override.yml.example" ]; then
+            echo "Creando archivo de override personalizado desde la plantilla..."
+            cp docker-compose.override.yml.example docker-compose.override.yml
+            echo "Por favor, revisa docker-compose.override.yml y ajusta según tu entorno de desarrollo."
+        else
+            echo "Error: No se encontró el archivo docker-compose.override.yml o un archivo docker-compose.override.yml.example dese el cual derivarlo."
+            exit 1
+        fi
+    fi
     echo "Modo desarrollo: montando volumenes para desarrollo rapido..."
     docker-compose -f docker-compose.yml -f docker-compose.override.yml up --build -d
 else
