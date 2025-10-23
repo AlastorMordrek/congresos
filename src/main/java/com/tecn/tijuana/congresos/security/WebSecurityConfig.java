022c4a58e947b6641e7b1fe8eeb3edd8b6d98fe1
Silas <el.silas@pm.me>
2025-10-23 11:09:11 -0700

    Se abrio la politica de CORS.

--- src/main/java/com/tecn/tijuana/congresos/security/WebSecurityConfig.java
+++ src/main/java/com/tecn/tijuana/congresos/security/WebSecurityConfig.java
@@ -20,6 +20,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication
   .UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -47,6 +52,8 @@ public class WebSecurityConfig {
     throws Exception {
     return http
       .csrf(AbstractHttpConfigurer::disable)
+
+      .cors(withDefaults())
 
       .authorizeHttpRequests(req ->
         req
@@ -92,6 +99,19 @@ public class WebSecurityConfig {
       .userDetailsService(userDetailsService)
 
       .build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+    configuration.setAllowedMethods(Arrays.asList("*"));
+    configuration.setAllowedHeaders(Arrays.asList("*"));
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
 
